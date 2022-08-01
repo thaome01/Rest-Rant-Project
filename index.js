59893d13caca6c4e724add805dd9c51e01fc6215
Meng Thao <mengthao023@gmail.com>
2022-08-01 13:22:19 -0500

    Adding page route

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+//error404 route//
+
+app.get('*', (req, res) => {
+    res.status(404)('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
