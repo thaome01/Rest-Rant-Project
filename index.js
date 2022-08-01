be7f58a26585468439e8278040314bca849929fe
Meng Thao <mengthao023@gmail.com>
2022-08-01 13:38:37 -0500

    Adding remaining stub rooutes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-//error404 route//
-
 app.get('*', (req, res) => {
-    res.status(404)('<h1>404 Page</h1>')
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
