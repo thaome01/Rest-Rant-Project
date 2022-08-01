13fa4d0af8e5de834ef5f48272cdc4a496bf197e
Meng Thao <mengthao023@gmail.com>
2022-08-01 16:24:15 -0500

    New places ca be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,29 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
+
 
 
 module.exports = router
