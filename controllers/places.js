ed5effc32a7fd0d0a77eca8da543cbd88fd1761c
Meng Thao <mengthao023@gmail.com>
2022-08-02 23:25:38 -0500

    Delete functionality implented

--- controllers/places.js
+++ controllers/places.js
@@ -5,11 +5,6 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-
 router.post('/', (req, res) => {
     if (!req.body.pic) {
         // Default image if one is not provided
@@ -25,6 +20,10 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -37,6 +36,37 @@ router.get('/:id', (req, res) => {
         res.render('places/show', { place: places[id], id })
     }
 })
+
+
+
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+})
+
+
 
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -51,6 +81,9 @@ router.delete('/:id', (req, res) => {
         res.redirect('/places')
     }
 })
+
+
+
 
 router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,31 +5,36 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <br></br>
+        <div>
+          <h1>{data.place.name}</h1>
+          <img src={data.place.pic} alt={data.place.name} />
+          <h3>
+            Located in {data.place.city}, {data.place.state}
+          </h3>
+        </div>
         <div>
           <h2>Rating</h2>
           <h5>Not rated</h5>
         </div>
         <br></br>
-        <br></br>
         <div>
+          <br />
           <h2>Description</h2>
-          <h5>
-            <p>Location in Phoenix, AZ and serving Coffee, Bakery</p>
-          </h5>
+          <h4>Serving {data.place.cuisines}</h4>
         </div>
-        <br></br>
         <br></br>
         <div>
           <h2>Comments</h2>
           <h5>No comments yet!</h5>
           <br></br>
           <br></br>
+        </div>
+        <div>
+          <a href="/places/${data.id}/edit" className="btn btn-warning">
+            Edit
+          </a>
+
           <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-              Edit
-            </a>
             <button type="submit" className="btn btn-danger">
               Delete
             </button>
@@ -41,3 +46,18 @@ function show(data) {
 }
 
 module.exports = show;
+
+// const React = require("react");
+// const Def = require("../default");
+
+// function show(data) {
+//   return (
+//     <Def>
+//       <main>
+//         <h1>{data.place.name}</h1>
+//       </main>
+//     </Def>
+//   );
+// }
+
+// module.exports = show;
